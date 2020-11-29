768970b54e400fc5461323cbf61a1072a5be83aa
Vinícius Mendes Lopes <69206@PROWAY.TREINA>
2020-11-29 16:08:30 -0300

    Adicionando o repositorio de local

---
+++ src/main/java/br/com/todolist/repositories/LocalRepository.java
@@ -0,0 +1,22 @@
+package br.com.todolist.repositories;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import br.com.todolist.model.Local;
+
+public class LocalRepository implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private EntityManager entityManager;
+
+	public List<Local> listarTudo() {
+		return entityManager.createQuery("from lcoal", Local.class).getResultList();
+	}
+
+}
